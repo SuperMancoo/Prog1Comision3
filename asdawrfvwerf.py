0f50651bad5739ac8921551a5aca41792e7838f9
BenitezNehuel <nehuelbenitez2961@gmail.com>
2024-10-30 21:30:40 -0300

    Modificacion de código

--- asdawrfvwerf.py
+++ asdawrfvwerf.py
@@ -1,8 +1,9 @@
-numero=input("Ingrese Un Numero")
+print("Ejercicio de prueba.")
+numero=input("Ingrese Un Numero:" )
 documento=open(f"Prog1Comision3\\Tabla-{numero}.txt")
 
 lineas = documento.readlines()
 for linea in lineas:
-    print(linea,end="")
+    print(linea)
 
 documento.close()
